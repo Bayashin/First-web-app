2f4438a965fd794c82c6cd1e4319d1ca21cb3316
Bayashin <k21099kk@aitech.ac.jp>
2022-01-31 23:37:43 +0900

    complete

--- todo.js
+++ todo.js
@@ -15,7 +15,8 @@ $(function() {
     function make_list(){
         get_document();
         num++;
-        $("#list_frame").append("<div class='level"+Level.value+"' id='list"+num+"'><span class='list_todo'>"+Things.value+"</span>"+Limit_d.value+"/"+Limit_t.value+"<button id='complete' class='complete' onclick='complete_list("+num+","+Level.value+")'>完了</button></div>");
+        $("#list_frame").append(
+            "<div class='level"+Level.value+"' id='list"+num+"'><span class='list_todo'>"+Things.value+"</span>"+Limit_d.value+"/"+Limit_t.value+"<button id='complete' class='complete' onclick='complete_list("+num+","+Level.value+")'>完了</button></div>");
         cancel_input();
     }
 
@@ -29,34 +30,26 @@ window.onload =function(){
     const inputElement = document.getElementById("fil");
     inputElement.addEventListener("change", handleFiles, false);
     function handleFiles() {
-        const fileList = this.files; /* ファイルリストを処理するコードがここに入る */
+        const fileList = this.files;
         const image = new Image;
         image.src = URL.createObjectURL(fileList[0]);
         document.getElementById("body").style.backgroundImage='url('+image.src+')';
     }
 }
 
-
 function complete_list(li_num,lv){
     let parent=document.getElementById("list"+li_num);
     parent.remove();
-    modalOpen(lv);
-}
-
-
-
-//ボタンがクリックされた時
-function modalOpen(lv) {
     const modal = document.getElementById("easyModal"+lv);
     modal.style.display = 'block';
-};
+}
 
 //バツ印がクリックされた時
 
 function modalClose(lv) {
     const modal = document.getElementById('easyModal'+lv);
     modal.style.display = 'none';
-};
+}
 
 //モーダルコンテンツ以外がクリックされた時
 addEventListener('click', outsideClose);
@@ -65,7 +58,7 @@ function outsideClose(e) {
     for(var i=0; i<modal.length; i++){
         if (e.target == modal[i]) {
     modal[i].style.display = 'none';
-    };
+    }
     }  
-};
+}
 
